370fdb5ced858c28779b94cf97170147ff220a6f
kuldeep singh <devkuldeeps89@gmail.com>
2021-12-26 15:29:34 +0530

    UI Implementation

---
+++ src/components/Slider.jsx
@@ -0,0 +1,119 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
+import styled from "styled-components";
+import { sliderItems } from "../data";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+
+  ${mobile({
+    display: "none",
+  })}
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.6;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.index * -100}vw);
+  transition: all 1.5s ease;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${(props) => props.bg};
+`;
+const ImageContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    const itemsLength = sliderItems.length - 1;
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : itemsLength);
+    } else {
+      setSlideIndex(slideIndex < itemsLength ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper index={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide key={item.id} bg={item.bg}>
+            <ImageContainer>
+              <Image src={item.img} />
+            </ImageContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
